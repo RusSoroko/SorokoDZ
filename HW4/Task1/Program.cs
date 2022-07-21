773c02808c2c877f991142067640fa67b01243b4
Ruslan Soroko <rus.soroko@gmail.com>
2022-07-21 11:12:06 +0300

    HW4 completed

---
+++ HW4/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность массива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Введите элементы массива: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+
+            for (int i = 1; i < n;)
+            {
+                sum += arr[i];
+                i += 2;
+            }
+            Console.WriteLine($"Сумма элементов массива с нечетными индексами: {sum}");
+
+            int min = arr[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine($"Минимальный элемент в массиве: {min}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ HW4/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность массива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Введите элементы массива: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Stack<int> stack = new Stack<int>();
+            
+            for (int i = 0; i < n; i++)
+            { 
+                stack.Push(arr[i]); 
+            }
+
+            foreach (int number in stack)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine();
+
+            int sc = stack.Count;
+
+            for (int i = 0; i < sc; i++)
+            {
+                Console.Write($"{stack.Pop()} ");
+            }
+        }
+    }
+}
