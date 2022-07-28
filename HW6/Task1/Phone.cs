361459ecf3b7b915f0689a9d12787b5051491c95
Ruslan Soroko <rus.soroko@gmail.com>
2022-07-28 18:01:52 +0300

    HW6 complete

---
+++ HW6/Task1/Phone.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Phone
+    {
+        private string _number;
+        private string _model;
+        private double _weight;
+
+        public Phone(string number, string model, double weight)
+        {
+            Number = number;
+            Model = model;
+            Weight = weight;
+        }
+
+        public string Number
+        {
+            get;
+            set;
+        }
+
+        public string Model
+        {
+            get;
+            private set;
+        }
+
+        public double Weight
+        {
+            get
+            {
+                return _weight;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    _weight = 0;
+                }
+                else
+                {
+                    _weight = value;
+                }
+            }
+        }
+
+        public void RecieveCall(string name)
+        {
+            Console.WriteLine($"Вам звонит {name}");
+        }
+    }
+}
---
+++ HW6/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone("1234567", "Nokia", 56.33);
+            Phone phone2 = new Phone("7654321", "Samsung", -56.33);
+
+            Console.WriteLine($"Номер: {phone1.Number}, модель: {phone1.Model}, вес: {phone1.Weight}");
+            Console.WriteLine($"Номер: {phone2.Number}, модель: {phone2.Model}, вес: {phone2.Weight}");
+
+            phone1.Number = "0123456";
+            //phone1.Model = "Apple";
+            //phone1.Weight = 3.98;
+
+            phone2.RecieveCall("Alex");
+        }
+    }
+}
